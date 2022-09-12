a6bcb9b968403a5681d2f19dd9829cbfff10b803
VMW4RE <victorcorreia88@gmail.com>
2022-09-12 14:12:06 -0300

    Projeto novo adicionado

---
+++ ALFAERP/src/alfaerp2/ALFAERP2.java
@@ -0,0 +1,13 @@
+package alfaerp2;
+
+import Forms.frmLogin;
+
+public class ALFAERP2 {
+
+    public static void main(String[] args) {
+        frmLogin Login = new frmLogin();
+        Login.setLocationRelativeTo(null);
+        Login.setVisible(true);
+    }
+    
+}
